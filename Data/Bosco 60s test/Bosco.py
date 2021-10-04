dea6e9bbfee367412247f1779687434163684bf8
XaviGithubCF <xavier.cano-ferrer@crick.ac.uk>
2021-10-04 14:21:51 +0100

    Data update

--- Data/Bosco 60s test/Bosco.py
+++ Data/Bosco 60s test/Bosco.py
@@ -13,16 +13,6 @@ t = tms/1000;
 s_mm = encoder_raw*mmpp # Displacement in mm
 s = s_mm/1000 # Displacement in mm
 
-
-
-# Find the minimum
-minimum = np.min(s)
-print(minimum)
-
-# Add this offset to the displacement to get a positive displacement
-
-s = s + abs(minimum)
-
 # Calculate speed in mm/s
 ds = np.gradient(s,axis=0)
 dt = np.gradient(t,axis=0)
@@ -32,7 +22,8 @@ v = ds/dt
 plt.plot(t,s)
 plt.ylabel('Displacement (m)')
 plt.xlabel('Time (s)')
-plt.ylim(0, 0.7)
+plt.title('Displacement')
+plt.ylim(-0.25, 0.4)
 plt.xlim(40, 115)
 plt.show()
 
@@ -41,6 +32,7 @@ plt.plot(t,v)
 plt.ylabel('Velocity (m/s)')
 plt.xlabel('Time (s)')
 #plt.ylim(-0.25, 0.32)
+plt.title('Velocity')
 plt.xlim(40, 115)
 plt.show()
 
@@ -50,20 +42,45 @@ P = F*v
 
 # Plot power
 plt.plot(t,P)
-plt.ylabel('Power W')
+plt.ylabel('Power (W)')
 plt.xlabel('Time (s)')
 #plt.ylim(-0.25, 0.32)
+plt.title('Power peak detection')
 plt.xlim(40, 115)
 plt.show()
 
 P = P.flatten()
+P = P[4500:11000]
 
 # Find peaks on the power signal
-peaks, _ = find_peaks(P, height=700)
+peaks, _ = find_peaks(P, height=700, distance=100)
 plt.plot(P)
 plt.plot(peaks, P[peaks], "P")
-plt.ylabel('Power W')
-plt.xlabel('Time (s)')
-plt.xlim(4500, 11000)
+plt.ylabel('Power (W)')
+plt.xlabel('Samples')
+plt.title('Peaks')
+#plt.xlim(4500, 11000)
 #plt.plot(np.zeros_like(P), "--", color="gray")
+plt.show()
+
+# Barplot of the peaks
+
+N_peaks = np.arange(len(peaks))
+#plt.plot(peaks, P[peaks], "P")
+#plt.plot(peaks, P[peaks])
+#plt.scatter(peaks, P[peaks])
+plt.bar(N_peaks, P[peaks])
+plt.ylabel('Power (W)')
+plt.xlabel('Jumps')
+plt.title('Bosco jump test')
+plt.show()
+
+# Relative power peaks
+
+P_rel = P[peaks]/m
+
+plt.bar(N_peaks, P_rel)
+plt.ylabel('Relative power (W/kg)')
+plt.xlabel('Jumps')
+plt.title('Bosco jump test')
 plt.show()
